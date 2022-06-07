87cb8fc588f67da1f32f4c10533d45f1e1c53ed1
Laura Lajara <Lajaralaura@outlook.es>
2022-06-06 23:20:55 -0400

    First commit

---
+++ android/app/src/main/kotlin/com/example/rapi_deli_market/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rapi_deli_market
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = const Color(0xFF388E3C);
+  static final Color iconColor1 = const Color(0xFFFFEB3B);
+  static final Color iconColor2 = const Color(0xFFFFF59D);
+  static final Color paraColor = const Color(0xFFE8F5E9);
+  static final Color buttonBackgroundColor = const Color(0xFFFAFAFA);
+  static final Color signColor = const Color(0xFFBDBDBD);
+  static final Color titleColor = const Color(0xFF303030);
+  static final Color mainBlackColor = const Color(0xFF212121);
+  static final Color yellowColor = const Color(0xFF558B2F);
+}
