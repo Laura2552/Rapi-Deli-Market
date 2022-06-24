414bc499ef7d1ad82293f7f8353e079fa2898ec1
Laura Lajara <Lajaralaura@outlook.es>
2022-06-23 23:21:07 -0400

    Intento de continuar 1

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -4,6 +4,7 @@ import 'package:rapi_deli_market/utils/colors.dart';
 import 'package:rapi_deli_market/widgets/big_text.dart';
 import 'package:rapi_deli_market/widgets/icon_and_text_widget.dart';
 import 'package:rapi_deli_market/widgets/small_text.dart';
+
 //karla
 class FoodPageBody extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   double _height = 220;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     pageController.addListener(() {
       setState(() {
@@ -27,7 +28,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   @override
-  void disponse(){
+  void disponse() {
     pageController.dispose();
   }
 
@@ -36,56 +37,54 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     return Container(
       height: 320,
       child: PageView.builder(
-        controller: pageController,
-        itemCount: 5,
-          itemBuilder: (context, position){
-        return _buildPageItem(position);
-      }),
+          controller: pageController,
+          itemCount: 5,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
     );
   }
-  Widget _buildPageItem(int index){
-    Matrix4 matrix = new Matrix4.identity();
-    if (index == _currPageValue.floor()){
-      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
-      var currTrans =_height*(1-currScale)/2;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
 
-    } 
-    else if (index == _currPageValue.floor()+1){
-      var currScale =_scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
-      var currTrans =_height*(1-currScale)/2;
+  Widget _buildPageItem(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
-    }
-    else if (index == _currPageValue.floor()-1){
-      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
-      var currTrans =_height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
-    }
-    else{
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
       var currScale = 0.8;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
     }
-    
+
     return Transform(
       transform: matrix,
       child: Stack(
         children: [
-        Container(
-        height: 220,
-        margin: EdgeInsets.only(left: 10, right: 10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven?Color(0xFF69c5df): Color(0xFF9294cc),
-            image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage(
-                    "assets/image/congelados.jpg"
-                )
-            )
-        ),
-      ),
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/image/congelados.jpg"))),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
@@ -96,46 +95,70 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                   color: Colors.white,
                   boxShadow: [
                     BoxShadow(
-                      color: Color(0xFFe8e8e8),
-
-                    )
-                  ]
-              ),
+                        color: Color(0xFFe8e8e8),
+                        blurRadius: 5.0,
+                        offset: Offset(0, 5)),
+                    BoxShadow(
+                        color: Colors.white,
+                        offset: Offset(-5, 0)
+                    ),
+                    BoxShadow(color: Colors.white,
+                        offset: Offset(5, 0)
+                    ),
+                  ]),
               child: Container(
                 padding: EdgeInsets.only(top: 15, left: 15, right: 15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     BigText(text: "Congelados"),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: [
                         Wrap(
-                          children: List.generate(5, (index) => Icon(Icons.star, color:AppColors.mainColor, size: 15,)),
+                          children: List.generate(
+                              5,
+                              (index) => Icon(
+                                    Icons.star,
+                                    color: AppColors.mainColor,
+                                    size: 15,
+                                  )),
                         ),
-                        SizedBox(width: 10,),
+                        SizedBox(
+                          width: 10,
+                        ),
                         SmallText(text: "4.5"),
-                        SizedBox(width: 10,),
+                        SizedBox(
+                          width: 10,
+                        ),
                         SmallText(text: "1287"),
-                        SizedBox(width: 10,),
+                        SizedBox(
+                          width: 10,
+                        ),
                         SmallText(text: "comentarios"),
                       ],
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: [
                         IconAndTextWidget(
-                          icon: Icons.check_circle,
-                          text: "Normal",
-                          iconColor: AppColors.iconColor1),
-
-                        IconAndTextWidget(icon: Icons.location_on,
-                        text: "1.7km",
-                        iconColor: AppColors.mainColor,),
-
-                        IconAndTextWidget(icon: Icons.access_time,
+                            icon: Icons.check_circle,
+                            text: "Normal",
+                            iconColor: AppColors.iconColor1),
+                        IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7km",
+                          iconColor: AppColors.mainColor,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.access_time,
                           text: "32min",
-                          iconColor: AppColors.iconColor2,)
+                          iconColor: AppColors.iconColor2,
+                        )
                       ],
                     )
                   ],
